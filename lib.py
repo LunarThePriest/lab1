adb13ccd7f79c0b27f3f1e5e2045cf623c2acd27
LunarThePriest <nastya7ka@gmail.com>
2026-09-08 20:43:52 +0300

    first commit

---
+++ lib.py
@@ -0,0 +1,11 @@
+def remainder_of(bigger, smaller):
+    return (bigger % smaller)
+
+def do_something():
+    for i in range(3, 0, -1):
+        print(f"{i}...")
+    print("...\nSomething happened.")
+    return
+
+def sing_phantom_of_the_opera():
+    print("The phantom of the opera is there,\nInside your mind.")
---
+++ main.py
@@ -0,0 +1,11 @@
+import lib
+
+lib.do_something()
+print()
+
+lib.sing_phantom_of_the_opera()
+print()
+
+a = 39
+b = 5
+print(f"The remainder of {a} divided by {b} is {lib.remainder_of(39, 5)}")
