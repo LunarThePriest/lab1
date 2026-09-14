be5ee967d07ab6cd068a5baca5b70dd2cec2edeb
LunarThePriest <nastya7ka@gmail.com>
2026-09-14 10:38:07 +0300

    b

---
+++ src/lib.py
@@ -0,0 +1,15 @@
+def remainder_of(bigger, smaller):
+    return (bigger % smaller)
+
+def do_something():
+    for i in range(3, 0, -1):
+        print(f"{i}...")
+    print("...\nSomething happened.")
+    return
+
+def sing_phantom_of_the_opera():
+    print("The phantom of the opera is there,\nInside your mind.")
+
+def average_of(numbers: list):
+    output = sum(numbers)/len(numbers)
+    return output
